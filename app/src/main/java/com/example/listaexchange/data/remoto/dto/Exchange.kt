37b533f2dd2251fccf10d75d73f86d604d502659
Kelvin Peña <kelvincastillo9911@gmail.com>
2022-06-22 16:54:54 -0400

    Creando la data class de Exchange

---
+++ app/src/main/java/com/example/listaexchange/data/remoto/dto/Exchange.kt
@@ -0,0 +1,8 @@
+package com.example.listaexchange.data.remoto.dto
+
+data class Exchange(
+    val name: String,
+    val description: String,
+    val active: Boolean,
+    val last_update: String,
+)
